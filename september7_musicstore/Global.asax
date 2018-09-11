a1fefb8314c9613d377b6bfeba19a7deb9c4cc2f
AscotBailey-2 <vvatsurin@smashreality.com>
2018-09-11 16:50:00 -0400

    Add project files.

---
+++ september7_musicstore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="september7_musicstore.MvcApplication" Language="C#" %>
---
+++ september7_musicstore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(september7_musicstore.Startup))]
+namespace september7_musicstore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
